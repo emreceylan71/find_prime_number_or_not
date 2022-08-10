f9ce2fcc6449504a7bab6f6caf2d2c2d8ae65774
emreceylan71 <109828939+emreceylan71@users.noreply.github.com>
2022-08-10 14:50:29 +0300

    Add files via upload

---
+++ prime_number.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int l,a;
+	cout << "You can find prime numbers .\nDo you want to find prime number you choose? \nPlease enter number : ";
+	cin >> l ;
+
+
+	for (int i = 2; i < l; i++) {
+		a= l % i ;
+		if (a == 0) {
+			cout << "This is not prime number" << endl;
+			return 0;
+			
+		}
+
+	}
+
+	cout << "Yes. This is prime number.";
+	
+		
+	
+}
+
